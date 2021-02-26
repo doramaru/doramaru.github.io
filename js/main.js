3836646620b22b1c7e8715d43357c78aff7fb21a
doramaru <77657509+doramaru@users.noreply.github.com>
2021-02-26 15:32:43 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+'use strict';
+
+{
+  const images = [
+    'img/DVC00007.JPG',
+    'img/DVC00008.JPG',
+    'img/DVC00009.JPG',
+    'img/DVC00018.JPG',
+    'img/DVC00037.JPG',
+    'img/DVC00049.JPG',
+    'img/DVC00061.JPG',
+    'img/DVC00072.JPG',
+   
+  ];
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById('main');
+  mainImage.src = images[currentIndex];
+
+  images.forEach((image, index) => {
+    const img = document.createElement('img');
+    img.src = image;
+    
+    const li = document.createElement('li');
+    if(index ===currentIndex) {
+      li.classList.add('current')
+    }
+    li.addEventListener('click', () =>{
+      mainImage.src = image;
+      const thumbnails = document.querySelectorAll('.thumbnails > li');
+      thumbnails[currentIndex].classList.remove('current');
+      currentIndex = index;
+      thumbnails[currentIndex].classList.add('current');
+    });
+
+    li.appendChild(img);
+    document.querySelector('.thumbnails').appendChild(li);
+
+  });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click' ,() => {
+    let target = currentIndex + 1;
+    if (target ===images.length) {
+      target = 0;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+  const prev = document.getElementById('prev');
+  prev.addEventListener('click' ,() => {
+    let target = currentIndex - 1;
+    if (target ===images.length - 9){
+      target = images.length - 1;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
+
+  let timeoutId;
+
+  function playSlideshow(){
+    timeoutId = setTimeout(() =>{
+      next.click();
+    playSlideshow();
+    },2000);
+  }
+
+  let isPlaying = false;
+
+  const play = document.getElementById('play');
+  play.addEventListener('click' ,() =>{
+    if (isPlaying === false){
+      playSlideshow();
+      play.textContent = 'Pause';
+    }else{
+      clearTimeout(timeoutId);
+      play.textContent = 'Play';
+
+    }
+    isPlaying = !isPlaying;
+  });
+}
